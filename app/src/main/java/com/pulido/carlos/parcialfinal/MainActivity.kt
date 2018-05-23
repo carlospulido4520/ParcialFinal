0a0e10799fbc74611e9ae62609f5a4e019349a09
carlospulido4520 <carlospulido4520@gmail.com>
2018-05-23 14:46:15 -0500

    Initial commit

---
+++ app/src/main/java/com/pulido/carlos/parcialfinal/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.pulido.carlos.parcialfinal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
